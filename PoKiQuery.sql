5fd2bac99e0b72e08444bb02312a9df862247a67
Brandon Hill <brandonjon26@gmail.com>
2021-06-03 14:09:54 -0500

    steps 6-10 are complete

--- PoKiQuery.sql
+++ PoKiQuery.sql
@@ -20,9 +20,44 @@ ORDER BY Name;
 
 
 --5.
-SELECT  TOP 76 a.Name, a.Id
+SELECT  TOP 76 a.Name author, g.Name grade, a.Id
 	FROM Author a
-		LEFT JOIN Grade g On g.Id = a.GradeId
-ORDER BY Name;
+		LEFT JOIN Grade g ON a.GradeId = g.Id
+ORDER BY a.Name;
+
+
+--6.
+SELECT  TOP 76 a.Name author, g.Name grade, n.Name gender, a.Id
+	FROM Author a
+		LEFT JOIN Grade g ON g.Id = a.GradeId
+		LEFT JOIN Gender n ON n.Id = a.GenderId
+ORDER BY a.Name;
+
+
+--7.
+SELECT sum(p.WordCount) '# of words'
+FROM Poem p;
+
+
+--8.
+SELECT TOP 1 p.Title, p.CharCount
+	FROM Poem p
+ORDER BY p.CharCount;
+
+
+--9.
+SELECT count(a.Name) '# of 3rd grade authors'
+	FROM Author a
+		LEFT JOIN Grade g ON g.Id = a.GradeId
+	WHERE g.Name = '3rd Grade';
+
+
+--10.
+SELECT count(a.Name) '# of 1st-3rd grade authors'
+	FROM Author a
+		LEFT JOIN Grade g ON g.Id = a.GradeId
+	WHERE g.Name = '1st grade' OR g.Name = '2nd grade' OR g.Name = '3rd grade';
+
+
 
 
